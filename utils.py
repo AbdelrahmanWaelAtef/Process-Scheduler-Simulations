9ec3a8d3cc2561951b5695ba1f8906e8b6ae2d15
Abdelrahman Wael <abdelrahman-wael@outlook.com>
2023-12-18 15:00:30 +0200

    Added more utility functions
    Implemented getArrivedProcesses, and checkIO functions.

--- utils.py
+++ utils.py
@@ -14,8 +14,8 @@ def initializeProcessStack(num_processes: int = 50, max_arrival_time: int = 50, 
         max_arrival_time (int): The maximum arrival time for generated processes (default is 50).
         min_duration (int): The minimum duration for generated processes (default is 10).
         max_duration (int): The maximum duration for generated processes (default is 50).
-        min_probability_io (float): The minimum probability of IO for generated processes (default is 0.0).
-        max_probability_io (float): The maximum probability of IO for generated processes (default is 0.25).
+        min_probability_io (float): The minimum probability of I/O for generated processes (default is 0.0).
+        max_probability_io (float): The maximum probability of I/O for generated processes (default is 0.25).
 
     Returns:
         Stack: A stack of Process objects representing the generated processes, sorted by arrival time.
@@ -29,10 +29,44 @@ def initializeProcessStack(num_processes: int = 50, max_arrival_time: int = 50, 
     stack.sort()
     return stack
 
+def getArrivedProcesses(stack: Stack, time_step:int):
+    """
+    Gets the processes the arrives at the given time step, and removes the processes from the stack.
+
+    Arguments:
+        stack (Stack): Stack of initialized processes.
+        time_step (int): The current the time step.
+
+    Returns:
+        List(Process): A list of the processes that arrives at that time step.
+    """
+    processes = []
+    while stack.peak().arrival_time == time_step:
+        processes.append(stack.pop())
+    return processes
+
+def checkIO(process:Process):
+    """
+    Checks if the process will perform an I/O operation or not.
+
+    Arguments:
+        process (Process): Examined process.
+
+    Returns:
+        bool: True if the process will perform I/O, False if not.
+    """
+    io = False
+    rand_num = random.uniform(0, 1)
+    if rand_num <= process.probability_io:
+        io = True
+    return io
+
 # Debug
 if __name__ == "__main__":
     stack = initializeProcessStack()
-    print("Process Name\tProcess Arival Time\tProcess Duration\tProcess Probability I/O")
+    print("Process Name\tProcess Arival Time\tProcess Duration\tProcess Probability I/O\t\tHave preformed I/O")
+
     for i in range(50):
-        process = stack.pop()
-        print(f"{process.name}\t\t{process.arrival_time}\t\t\t{process.duration}\t\t\t{process.probability_io}")
+        processes = getArrivedProcesses(stack, i)
+        for process in processes:
+            print(f"{process.name}\t\t{process.arrival_time}\t\t\t{process.duration}\t\t\t{process.probability_io}\t\t{checkIO(process)}")
